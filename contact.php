79ba587996eaf3e2a9241474c966931887dedf63
psyloute <p1991gv@gmail.com>
2019-02-25 11:18:49 +0100

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,42 @@
+<?php
+// On vérifie que tous les champs du formulaire sont bien remplis
+if (isset($_POST['submit']) && isset($_POST['name']) && isset($_POST['object']) && isset($_POST['email']) && isset($_POST['message'])) {
+
+    // On récupère chacun des champs du formulaire
+    $name = $_POST['name'];
+    $object = $_POST['object'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // On se connecte à la base de données MySQL
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=pia;charset=utf8', 'root', '');
+
+    } catch (Exception $e) {
+        // En cas d'erreur, on s'arrête là et on affiche l'erreur
+        die('Erreur : ' . $e->getMessage());
+    }
+
+    echo 'Envoi du message... ';
+
+    // On envoie les données à la base de données. On fait ça en 2 temps pour éviter les failles SQL (injection SQL)
+    // D'abord on prépare la requete, mais on met pas directement les vraies valeurs, on les nomme juste (avec le :).
+    $req = $bdd->prepare('INSERT INTO messages(pseudo, object, email, message) VALUES (:name, :object, :email, :message)');
+    // Ensuite, on envoie vraiment la requete, en précisant que tel paramètre a telle valeur.
+    $res = $req->execute(array(
+        'name' => $name,
+        'object' => $object,
+        'email' => $email,
+        'message' => $message
+    ));
+
+    // On vérifie que l'envoi de données s'est bien passé
+    if ($res) {
+    echo 'Message envoyé !';
+    // On redirige sur la page "contact.html"
+    header("Location: contact.html");
+    } else {
+        // Si je veux que ça redirige aussi en cas d'erreur, j'enlève le if / else et je garde juste le 'header(...)'
+        echo 'Echec de l\'envoi du message';
+    }
+}
