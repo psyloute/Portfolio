e2e96e0b3bec3b820288da24acc98eaef6e2350c
psyloute <p1991gv@gmail.com>
2019-02-28 16:02:43 +0100

    Update copyright.js

--- js/copyright.js
+++ js/copyright.js
@@ -1,3 +1,4 @@
+// Syntaxe qui permet de stocker la date
 var LaDate = new Date();
-// je définis la valeur dans le <span> en recuperant son id et en y insérant du code html via le script js
+// On définit la valeur dans le <span> en recuperant son id et en y insérant du code html via le script js
 document.getElementById("DateCopyright").innerHTML = ( "©" + " " + LaDate.getFullYear() + " " + "Copyright - Gippet-Vinard Priscillia");
