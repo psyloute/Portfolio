1c2c064c5b14369ee7f06486931c1f12b766e618
psyloute <p1991gv@gmail.com>
2019-02-28 16:00:28 +0100

    Update copyright.js

--- js/copyright.js
+++ js/copyright.js
@@ -1,2 +1,3 @@
 var LaDate = new Date();
+// je définis la valeur dans le <span> en recuperant son id et en y insérant du code html via le script js
 document.getElementById("DateCopyright").innerHTML = ( "©" + " " + LaDate.getFullYear() + " " + "Copyright - Gippet-Vinard Priscillia");
