b440bc783d7b3adb2ffb6c8f592ce58f294c0ebf
psyloute <p1991gv@gmail.com>
2019-03-04 09:51:22 +0100

    Create btntop.js

---
+++ js/btntop.js
@@ -0,0 +1,11 @@
+// J'ai choisit de rajouter script qui fait apparaitre un bouton "top" quand l'utilisateur descend la page de 35 pixel
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 35 || document.documentElement.scrollTop > 35) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
