e3c68ab1d80d1c3f267c8f366a783d7fd4efb7ea
psyloute <p1991gv@gmail.com>
2019-03-04 11:01:40 +0100

    Update copyright.js

--- js/copyright.js
+++ js/copyright.js
@@ -1,4 +1,6 @@
-// Syntaxe qui permet de stocker la date
+// Script qui va permettre a la date sur le copyright de changer seule avec les années
+
+// Syntaxe qui permet de stocker la date :
 var LaDate = new Date();
 // On définit la valeur dans le <span> en recuperant son id et en y insérant du code html via le script js
 document.getElementById("DateCopyright").innerHTML = ( "©" + " " + LaDate.getFullYear() + " " + "Copyright - Gippet-Vinard Priscillia");
