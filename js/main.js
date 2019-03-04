d2098c4ea1e1c505e40f13d165913369872f0b3c
psyloute <p1991gv@gmail.com>
2019-03-04 10:03:05 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /* Start Bootstrap - Copyright 2013-2018 Start Bootstrap */
 
- //Script natif pour l'effet de scroll lent et les petites fenêtres "ils m'ont fait confiance" :
+ //Script natif pour l'effet de scroll lent et les petites fenêtres "ils m'ont fait confiance" 
+ // /!\ je n'ai pas produit ce code moi même, je l'ai simplement intégrer /!\ :
 
 ! function(o) {
     "use strict";
