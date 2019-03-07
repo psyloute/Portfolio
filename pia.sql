816f865f6ba24db768aed28f8f2f0f4ddcf58601
psyloute <p1991gv@gmail.com>
2019-03-07 11:50:41 +0100

    Update pia.sql

--- pia.sql
+++ pia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 25 fév. 2019 à 11:09
+-- Généré le :  jeu. 07 mars 2019 à 10:49
 -- Version du serveur :  5.7.19
 -- Version de PHP :  5.6.31
 
@@ -35,13 +35,6 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `email` varchar(535) NOT NULL,
   `message` longtext NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `messages`
---
-
-INSERT INTO `messages` (`pseudo`, `object`, `email`, `message`) VALUES
-('Priscillia', 'Test', 'pia@pia.com', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
